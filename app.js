4027ffacfd786ec1edd2ca3cddf0b69bd07d5f60
vmathu02 <vivek.mathur@tufts.edu>
2020-06-17 00:16:46 +0530

    cors

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var http = require("http");
 var socketIo = require("socket.io");
 var { nanoid } = require("nanoid");
 var cors = require("cors");
-os = require('os'),
-    get_players = require("./test.js");
-
+get_players = require("./test.js");
 
 
 // key: id, value: room dict
@@ -17,63 +15,6 @@ rooms = {};
 
 // create express app
 var app = express();
-
-// hard coded configuration object
-conf = {
-    // look for PORT environment variable,
-    // else look for CLI argument,
-    // else use hard coded value for port 9000
-    port: process.env.PORT || process.argv[2] || 9000,
-
-    // origin undefined handler
-    // see https://github.com/expressjs/cors/issues/71
-    originUndefined: function(req, res, next) {
-
-        if (!req.headers.origin) {
-
-            res.json({
-
-                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
-
-            });
-
-        } else {
-
-            next();
-
-        }
-
-    },
-
-    // Cross Origin Resource Sharing Options
-    cors: {
-
-        // origin handler
-        origin: function(origin, cb) {
-
-            // setup a white list
-            let wl = ['https://dustinpfister.github.io'];
-
-            if (wl.indexOf(origin) != -1) {
-
-                cb(null, true);
-
-            } else {
-
-                cb(new Error('invalid origin: ' + origin), false);
-
-            }
-
-        },
-
-        optionsSuccessStatus: 200
-
-    }
-
-};
-
-// use origin undefined handler, then cors for all paths
-app.use(conf.originUndefined, cors(conf.cors));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
